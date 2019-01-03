8fa79de8cc068e7c740ba8f6b474e259e6f105a6
John O Keeffe <jokeeffe09@gmail.com>
2019-01-03 17:03:22 +0000

    Add code to play sound on key press

--- src/App.js
+++ src/App.js
@@ -2,30 +2,39 @@ import React, { Component } from 'react';
 import './App.css';
 
 const notes = [{
+    keyCode: 81,
     name: 'heater-1',
     url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
   },{
+    keyCode: 87,
     name: 'heater-2',
     url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
   },{
+    keyCode: 69,
     name: 'heater-3',
     url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
   },{
+    keyCode: 65,
     name: 'heater-4',
     url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
   },{
+    keyCode: 83,
     name: 'heater-6',
     url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
   },{
+    keyCode: 68,
     name: 'punchy-kick-1',
     url: 'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3'
   },{
+    keyCode: 90,
     name: 'kick-n-hat',
     url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
   },{
+    keyCode: 88,
     name: 'kick-1',
     url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
   },{
+    keyCode: 67,
     name: 'h1',
     url: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3'
   }];
@@ -35,13 +44,26 @@ class App extends Component {
     super(props)
   }
 
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyPress);
+  }
+
   handleClick = (event) => {
     console.log(event.target.value);
     this.playSound(event.target.value);
   }
 
-  playSound = (url) => {
-    const sound = new Audio(url).play();
+  handleKeyPress = (event) => {
+    this.playSound(event.keyCode);
+  }
+
+  playSound = (key) => {
+    console.log(key);
+    const note = notes.find(obj => obj.keyCode == key);
+    if(note !== undefined) {
+      console.log(note);
+      const sound = new Audio(note.url).play();
+    }
   }
 
   render() {
@@ -60,23 +82,23 @@ class PadContainer extends Component {
   render() {
     return (
       <div className="pad-container">
-        <DrumPad id={notes[0].name} url={notes[0].url} value={"Q"} 
+        <DrumPad id={notes[0].name} url={notes[0].url} value={"Q"} keyCode={81}
           onClick={this.props.onClick}/>
-        <DrumPad id={notes[1].name} url={notes[1].url} value={"W"} 
+        <DrumPad id={notes[1].name} url={notes[1].url} value={"W"} keyCode={87} 
           onClick={this.props.onClick}/>
-        <DrumPad id={notes[2].name} url={notes[2].url} value={"E"}
+        <DrumPad id={notes[2].name} url={notes[2].url} value={"E"} keyCode={69}
           onClick={this.props.onClick}/>
-        <DrumPad id={notes[3].name} url={notes[3].url} value={"A"}
+        <DrumPad id={notes[3].name} url={notes[3].url} value={"A"} keyCode={65}
           onClick={this.props.onClick}/>
-        <DrumPad id={notes[4].name} url={notes[4].url} value={"S"}
+        <DrumPad id={notes[4].name} url={notes[4].url} value={"S"} keyCode={83}
           onClick={this.props.onClick}/>
-        <DrumPad id={notes[5].name} url={notes[5].url} value={"D"}
+        <DrumPad id={notes[5].name} url={notes[5].url} value={"D"} keyCode={68}
           onClick={this.props.onClick}/>
-        <DrumPad id={notes[6].name} url={notes[6].url} value={"Z"}
+        <DrumPad id={notes[6].name} url={notes[6].url} value={"Z"} keyCode={90}
           onClick={this.props.onClick}/>
-        <DrumPad id={notes[7].name} url={notes[7].url} value={"X"}
+        <DrumPad id={notes[7].name} url={notes[7].url} value={"X"} keyCode={88}
           onClick={this.props.onClick}/>
-        <DrumPad id={notes[8].name} url={notes[8].url} value={"C"}
+        <DrumPad id={notes[8].name} url={notes[8].url} value={"C"} keyCode={67}
           onClick={this.props.onClick}/>
       </div>
     );
@@ -86,7 +108,7 @@ class PadContainer extends Component {
 function DrumPad(props) {
 
   return (
-    <button className="drum-pad" onClick={props.onClick} value={props.url}>
+    <button className="drum-pad" onClick={props.onClick} value={props.keyCode}>
       {props.value}
       <audio src={props.url} type="audio/mpeg"></audio>
     </button>
