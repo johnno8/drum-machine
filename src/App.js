efc5cb2168ac337f04b942bf8e92958deb1c3923
John O'Keeffe <jokeeffe09@gmail.com>
2019-01-10 09:54:08 +0000

    Refactor to render drum pad by mapping over notes array

--- src/App.js
+++ src/App.js
@@ -2,76 +2,94 @@ import React, { Component } from 'react';
 import './App.css';
 
 const notes = [{
+    triggerKey: 'Q',
     keyCode: 81,
     name: 'heater-1',
     url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
   },{
+    triggerKey: 'W',
     keyCode: 87,
     name: 'heater-2',
     url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
   },{
+    triggerKey: 'E',
     keyCode: 69,
     name: 'heater-3',
     url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
   },{
+    triggerKey: 'A',
     keyCode: 65,
     name: 'heater-4',
     url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
   },{
+    triggerKey: 'S',
     keyCode: 83,
     name: 'heater-6',
     url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
   },{
+    triggerKey: 'D',
     keyCode: 68,
     name: 'punchy-kick-1',
     url: 'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3'
   },{
+    triggerKey: 'Z',
     keyCode: 90,
     name: 'kick-n-hat',
     url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
   },{
+    triggerKey: 'X',
     keyCode: 88,
     name: 'kick-1',
     url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
   },{
+    triggerKey: 'C',
     keyCode: 67,
     name: 'h1',
     url: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3'
   }];
 
 const notes1 = [{
+    triggerKey: 'Q',
     keyCode: 81,
     name: 'dsc_Oh',
     url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
   },{
+    triggerKey: 'W',
     keyCode: 87,
     name: 'side_stick_1',
     url: 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3'
   },{
+    triggerKey: 'E',
     keyCode: 69,
     name: 'cev_h2',
     url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
   },{
+    triggerKey: 'A',
     keyCode: 65,
     name: 'chord_1',
     url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3'
   },{
+    triggerKey: 'S',
     keyCode: 83,
     name: 'chord_2',
     url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3'
   },{
+    triggerKey: 'D',
     keyCode: 68,
     name: 'chord_3',
     url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3'
   },{
+    triggerKey: 'Z',
     keyCode: 90,
     name: 'give_us_a_light',
     url: 'https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3'
   },{
+    triggerKey: 'X',
     keyCode: 88,
     name: 'dry_ohh',
     url: 'https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3'
   },{
+    triggerKey: 'C',
     keyCode: 67,
     name: 'brk_snr',
     url: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3'
@@ -89,7 +107,8 @@ class App extends Component {
       power: true,
       mode: true,
       volume: .5,
-      color: mode1Color
+      color: mode1Color,
+      currentNotes: notes
     }
   }
 
@@ -113,8 +132,7 @@ class App extends Component {
 
   playSound = (key) => {
     console.log(key);
-    const currentNotes = this.state.mode ? notes : notes1;
-    const note = currentNotes.find(obj => obj.keyCode == key);
+    const note = this.state.currentNotes.find(obj => obj.keyCode == key);
     if(this.state.power && note !== undefined) {
       console.log(note);
       this.setState({
@@ -122,6 +140,7 @@ class App extends Component {
       });
       const sound = new Audio(note.url);
       sound.volume = this.state.volume;
+      sound.currentTime = 0;
       sound.play();
     }
   }
@@ -140,12 +159,14 @@ class App extends Component {
         this.setState({
           mode: !this.state.mode,
           displayName: '',
-          color: !this.state.mode ? mode1Color : mode2Color
+          color: !this.state.mode ? mode1Color : mode2Color,
+          currentNotes: !this.state.mode ? notes : notes1
         });    
       } else {
         this.setState({
           mode: !this.state.mode,
-          displayName: ''
+          displayName: '',
+          currentNotes: !this.state.mode ? notes : notes1
         });
       }
     }
@@ -170,7 +191,8 @@ class App extends Component {
         <PadContainer 
           className="pad-container"
           onClick={this.handleClick}
-          color={this.state.color}/>
+          color={this.state.color}
+          notes={this.state.currentNotes}/>
         <ControlsContainer 
           name={this.state.displayName}
           onClick={this.powerToggle}
@@ -187,24 +209,19 @@ class PadContainer extends Component {
   render() {
     return (
       <div className="pad-container">
-        <DrumPad id={notes[0].name} url={notes[0].url} value={"Q"} keyCode={81}
-          onClick={this.props.onClick} color={this.props.color}/>
-        <DrumPad id={notes[1].name} url={notes[1].url} value={"W"} keyCode={87} 
-          onClick={this.props.onClick} color={this.props.color}/>
-        <DrumPad id={notes[2].name} url={notes[2].url} value={"E"} keyCode={69}
-          onClick={this.props.onClick} color={this.props.color}/>
-        <DrumPad id={notes[3].name} url={notes[3].url} value={"A"} keyCode={65}
-          onClick={this.props.onClick} color={this.props.color}/>
-        <DrumPad id={notes[4].name} url={notes[4].url} value={"S"} keyCode={83}
-          onClick={this.props.onClick} color={this.props.color}/>
-        <DrumPad id={notes[5].name} url={notes[5].url} value={"D"} keyCode={68}
-          onClick={this.props.onClick} color={this.props.color}/>
-        <DrumPad id={notes[6].name} url={notes[6].url} value={"Z"} keyCode={90}
-          onClick={this.props.onClick} color={this.props.color}/>
-        <DrumPad id={notes[7].name} url={notes[7].url} value={"X"} keyCode={88}
-          onClick={this.props.onClick} color={this.props.color}/>
-        <DrumPad id={notes[8].name} url={notes[8].url} value={"C"} keyCode={67}
-          onClick={this.props.onClick} color={this.props.color}/>
+        {this.props.notes.map((note, i) => {
+            return(
+              <DrumPad
+                id={note.name}
+                url={note.url}
+                value={note.triggerKey}
+                keyCode={note.keyCode}
+                onClick={this.props.onClick}
+                color={this.props.color}
+              />
+            )
+          })
+        }
       </div>
     );
   }
